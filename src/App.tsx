fc1941be4d6c24980a9f79a2810602456426fb50
xulihang <xulihanghai@163.com>
2023-07-27 16:22:19 +0800

    add hideSelect prop for MRZScanner

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
       }
       {scanning && 
         <MRZScanner
+          hideSelect={true}
           onScanned={(results)=>(console.log(results))}
         >
           <button className='close-button' onClick={()=>setScanning(false)}>Close</button>
