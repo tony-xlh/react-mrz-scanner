59f6beba46e9b781a233af3e1c4f2d0bb2dc4ae2
xulihang <xulihanghai@163.com>
2023-07-27 15:49:22 +0800

    add a button to trigger scanning

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 import MRZScanner from './component/MRZScanner'
+import { useState } from 'react';
 
 function App() {
+  const [scanning, setScanning] = useState(false);
   return (
     <div>
-      React Demo
-      <MRZScanner></MRZScanner>
+      {!scanning &&
+        <>
+          <h2>MRZ Scanner</h2>
+          <button onClick={()=>setScanning(true)}>Start Scanning MRZ</button>
+        </>
+      }
+      {scanning && 
+        <MRZScanner></MRZScanner>
+      }
     </div>
   )
 }
