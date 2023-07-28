ffe64c72b94292f842e0ed700be58b8dbb33b8fc
xulihang <xulihanghai@163.com>
2023-07-28 10:07:44 +0800

    add progress modal

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App() {
   const [MRZLineResults, setMRZLineResults] = useState<DLRLineResult[]>([]);
   const [showScanner, setShowScanner] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState(false);
+  const [showProgress, setShowProgress] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const showConfirmationModal = (results:DLRLineResult[]) => {
     if (scanning === true) {
@@ -58,15 +60,32 @@ function App() {
         <MRZScanner
           scanning={scanning}
           hideSelect={true}
+          onResourcesLoadStarted={()=>setShowProgress(true)}
+          onResourcesLoadProgress={(_resourcesPath,_progress)=>{
+            const percent = (_progress.loaded/_progress.total*100).toFixed(2);
+            setProgress(parseInt(percent));
+          }}
+          onResourcesLoaded={()=>{
+            setShowProgress(false);
+            setProgress(0);
+          }}
           onScanned={(results)=>(showConfirmationModal(results))}
         >
           {showConfirmation && 
-            <div className="confirmation-modal">
+            <div className="confirmation modal">
               <div className="overflow">
                 <pre>{MRZString()}</pre>
               </div>
               <button onClick={()=>correct()}>Correct</button>
               <button onClick={()=>rescan()} >Rescan</button>
+            </div>
+          }
+          {showProgress && 
+            <div className="progress modal">
+              <div>Loading the model...</div>
+              <div>
+                Progress: <span>{progress}</span>%
+              </div>
             </div>
           }
           <button className="close-button" onClick={()=>setScanning(false)}>Close</button>
